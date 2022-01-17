9c76f7719c3c4da0ad171e3123017d6fe636c5b1
Xiang Fan <sfanxiang@gmail.com>
2022-01-17 02:18:48 -0500

    Version 0.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='saver',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(where='src'),
     package_dir={'': 'build', 'saver': 'src/saver'},
 )
